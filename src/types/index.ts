f2e9ed0ace5cd51924abbceb7d1f9e796143e866
MI Sajid <misajidsa@gmail.com>
2024-07-05 16:33:52 +0600

    feat: add getUserByUserId

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,17 +1,18 @@
-interface ISession {
+export interface ISession {
     id: string
     userId: string
     expiresAt: Date
 }
 
-type ErrorResponse = { success: false; message: string }
-type SessionResponse = { success: true; session: ISession } | ErrorResponse
-type UserResponse<T = unknown> = { success: true; user: T } | ErrorResponse
+export type ErrorResponse = { success: false; message: string }
+export type SessionResponse = { success: true; session: ISession } | ErrorResponse
+export type UserResponse<T = unknown> = { success: true; user: T } | ErrorResponse
 
-interface Adapter {
+export interface Adapter {
     createSession(opts: { userId: string; expiresIn: number }): Promise<SessionResponse>
     getSession(sessionId: string): Promise<SessionResponse>
     deleteSession(sessionId: string): Promise<SessionResponse>
-    getUser(sessionId: string): Promise<UserResponse>
+    getUserBySessionId(sessionId: string): Promise<UserResponse>
+    getUserByUserId(userId: string): Promise<UserResponse>
     deleteUsersAllSessions(sessionId: string): Promise<{ success: boolean; message: string }>
 }
