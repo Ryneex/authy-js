e0b23e324e8476964110ae8c8e0d563860b13059
MI Sajid <misajidsa@gmail.com>
2024-07-07 16:00:50 +0600

    feat: add generic types

--- src/types/index.ts
+++ src/types/index.ts
@@ -12,7 +12,7 @@ export interface Adapter {
     createSession(opts: { userId: string; expiresIn: number }): Promise<SessionResponse>
     getSession(sessionId: string): Promise<SessionResponse>
     deleteSession(sessionId: string): Promise<SessionResponse>
-    getUserBySessionId(sessionId: string): Promise<UserResponse>
-    getUserByUserId(userId: string): Promise<UserResponse>
+    getUserBySessionId<T>(sessionId: string): Promise<UserResponse<T>>
+    getUserByUserId<T>(userId: string): Promise<UserResponse<T>>
     deleteUsersAllSessions(sessionId: string): Promise<{ success: boolean; message: string }>
 }
